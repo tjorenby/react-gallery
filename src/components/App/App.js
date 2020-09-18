211cfaa9d87163ed5c0d0502cbb34f0e7e842b79
Tyler Jorenby <tyler.jorenby@gmail.com>
2020-09-18 18:22:33 -0500

    configured axios getImages() function on App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,35 @@
 import React, { Component } from 'react';
 import './App.css';
 import GalleryList from '../GalleryList/GalleryList';
+import axios from 'axios';
+
 
 class App extends Component {
+
+  state = {
+    images: []
+  }
+
+  componentDidMount = () => {
+    console.log('componentDidMount is ready');
+    this.getImages();
+  }
+
+  getImages = () => {
+    axios({
+      method: 'GET',
+      url: '/images'
+    }).then(response => {
+      console.log('back from GET with:', response);
+      console.log('response.data is:', response.data);
+      this.setState({
+        images: response.data
+      });
+    }).catch(err =>{
+      console.log('GET error!', err);
+    });
+
+  }
   render() {
     return (
       <div className="App">
