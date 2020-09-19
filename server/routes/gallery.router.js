7fd1165ba0bb4922fa7b78ba94a9366618125d75
Tyler Jorenby <tyler.jorenby@gmail.com>
2020-09-19 11:42:16 -0500

    db connections are set up and GET route is functioning

--- server/routes/gallery.router.js
+++ server/routes/gallery.router.js
@@ -1,24 +1,65 @@
 const express = require('express');
 const router = express.Router();
-const galleryItems = require('../modules/gallery.data');
+const pool = require('../modules/pool.js');
 
-// DO NOT MODIFY THIS FILE FOR BASE MODE
 
-// PUT Route
-router.put('/like/:id', (req, res) => {
-    console.log(req.params);
-    const galleryId = req.params.id;
-    for(const galleryItem of galleryItems) {
-        if(galleryItem.id == galleryId) {
-            galleryItem.likes += 1;
-        }
-    }
-    res.sendStatus(200);
-}); // END PUT Route
-
-// GET Route
-router.get('/', (req, res) => {
-    res.send(galleryItems);
-}); // END GET Route
+// GET route
+router.get('/images', (req,res) => {
+    const sqlText = `SELECT * FROM "images" ORDER BY "id", "title", "description", "path" DESC;`;
+    pool.query(sqlText)
+        .then(result => {
+            console.log('Got stuff back from Database', result);
+            res.send(results.rows);
+        })
+        .catch(error => {
+            console.log(`Error making Database query: ${sqlText}`, error);
+            res.sendStatus(500);
+        })
+})
 
 module.exports = router;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const galleryItems = require('../modules/gallery.data');
+
+// // DO NOT MODIFY THIS FILE FOR BASE MODE
+
+// // // PUT Route
+// // router.put('/like/:id', (req, res) => {
+// //     console.log(req.params);
+// //     const galleryId = req.params.id;
+// //     for(const galleryItem of galleryItems) {
+// //         if(galleryItem.id == galleryId) {
+// //             galleryItem.likes += 1;
+// //         }
+// //     }
+// //     res.sendStatus(200);
+// // }); // END PUT Route
+
+// // // GET Route
+// // router.get('/', (req, res) => {
+// //     res.send(galleryItems);
+// // }); // END GET Route
